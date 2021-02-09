ebb4983aca685b35874f09c3b973e426bad0f8d2
Eduardo San Martin Morote <posva13@gmail.com>
2021-02-09 10:34:11 +0100

    chore: up deps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export { createStore } from './store'
 export { setActiveReq, setStateProvider, getRootState } from './rootStore'
-export { StateTree, Store, StoreWithActions, StoreWithGetters, StoreWithState } from './types'
+export {
+  StateTree,
+  Store,
+  StoreWithActions,
+  StoreWithGetters,
+  StoreWithState,
+} from './types'
 export { PiniaSsr } from './ssrPlugin'
