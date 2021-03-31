5ea8a5d8c6d8739b34ea85ef2d827749be11b475
Eduardo San Martin Morote <posva13@gmail.com>
2021-03-31 11:09:42 +0200

    docs: docs for pinia plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,28 @@
 import { PluginFunction } from 'vue'
 import { piniaSymbol } from './rootStore'
 
+/**
+ * Vue Plugin that must be installed for pinia to work. Note **you don't need
+ * this plugin if you are using Nuxt.js**. Use the `buildModule` instead:
+ * https://pinia.esm.dev/ssr/nuxt.html.
+ *
+ * @example
+ * ```js
+ * import Vue from 'vue'
+ * import { PiniaPlugin, createPinia } from 'pinia'
+ *
+ * Vue.use(PiniaPlugin)
+ * const pinia = createPinia()
+ *
+ * new Vue({
+ *   el: '#app',
+ *   // ...
+ *   pinia,
+ * })
+ * ```
+ *
+ * @param _Vue
+ */
 export const PiniaPlugin: PluginFunction<void> = function (_Vue) {
   // Equivalent of
   // app.config.globalProperties.$pinia = pinia
