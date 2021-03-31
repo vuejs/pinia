7c0369194f6518ab5ea221f4a260f9b4506f222b
Eduardo San Martin Morote <posva13@gmail.com>
2021-03-31 16:10:15 +0200

    fix(nuxt): automatically transpile pinia

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,5 +1,6 @@
 import path from 'path'
 
+/** @type {import('@nuxt/types').Module<{ disableVuex?: boolean}>} */
 export default function NuxtPiniaModule(options) {
   const disableStore = 'disableVuex' in options ? options.disableVuex : true
 
@@ -12,4 +13,15 @@ export default function NuxtPiniaModule(options) {
     src: path.resolve(__dirname, 'plugin.js'),
     fileName: 'pinia.js',
   })
+
+  this.options.build.transpile = this.options.build.transpile || []
+
+  // transpile pinia if @vue/composition-api is transpiled because we must use the same instance
+  if (
+    !this.options.dev &&
+    !this.options.build.transpile.includes('pinia') &&
+    this.options.build.transpile.includes('@vue/composition-api')
+  ) {
+    this.options.build.transpile.push('pinia')
+  }
 }
