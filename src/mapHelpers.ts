5e3f12a5d1e4bb1f2c80ea6626453b91f0fbaa52
Eduardo San Martin Morote <posva13@gmail.com>
2021-04-06 19:52:56 +0200

    test: mapStores

--- src/mapHelpers.ts
+++ src/mapHelpers.ts
@@ -7,7 +7,7 @@ type StoreObject<S> = S extends StoreDefinition<
   infer Actions
 >
   ? {
-      [Id in Ids]: () => Store<Ids, State, Getters, Actions>
+      [Id in `${Ids}Store`]: () => Store<Id, State, Getters, Actions>
     }
   : {}
 
@@ -15,12 +15,32 @@ type Spread<A extends readonly any[]> = A extends [infer L, ...infer R]
   ? StoreObject<L> & Spread<R>
   : unknown
 
+/**
+ * Allows using stores without the composition API (`setup()`) by generating an object to be spread in the `computed` field of a component.
+ *
+ * @example
+ * ```js
+ * export default {
+ *   computed: {
+ *     // other computed properties
+ *     ...mapStores(useUserStore, useCartStore)
+ *   },
+
+ *   created() {
+ *     this.userStore // store with id "user"
+ *     this.cartStore // store with id "cart"
+ *   }
+ * }
+ * ```
+ *
+ * @param stores - list of stores to map to an object
+ */
 export function mapStores<Stores extends unknown[]>(
   ...stores: [...Stores]
 ): Spread<Stores> {
   return stores.reduce((reduced, useStore) => {
-    // @ts-ignore
-    reduced[useStore.$id] = function () {
+    // @ts-ignore: $id is added by defineStore
+    reduced[useStore.$id + 'Store'] = function () {
       return (useStore as GenericStoreDefinition)((this as any).$pinia)
     }
     return reduced
