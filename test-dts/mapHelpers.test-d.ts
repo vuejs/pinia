3df934a6432e069feaa42451f9d048e013abe6bc
Eduardo San Martin Morote <posva13@gmail.com>
2021-04-08 12:01:20 +0200

    test: add dts tests

---
+++ test-dts/mapHelpers.test-d.ts
@@ -0,0 +1,63 @@
+import { mapActions, mapState } from 'dist/src'
+import { defineStore, expectType, mapStores } from '.'
+
+const useStore = defineStore({
+  id: 'name',
+  state: () => ({ a: 'on' as 'on' | 'off', nested: { counter: 0 } }),
+  getters: {
+    upper() {
+      return this.a.toUpperCase()
+    },
+  },
+  actions: {
+    toggleA() {
+      this.a = this.a === 'off' ? 'on' : 'off'
+    },
+
+    setToggle(a: 'on' | 'off') {
+      return (this.a = a)
+    },
+  },
+})
+
+const useCounter = defineStore({
+  id: 'counter',
+  state: () => ({ n: 0 }),
+})
+
+const useStoreDos = defineStore({
+  id: 'dos',
+  state: () => ({}),
+})
+
+type MainStore = ReturnType<typeof useStore>
+type DosStore = ReturnType<typeof useStoreDos>
+type CounterStore = ReturnType<typeof useCounter>
+
+const computedStores = mapStores(useStore, useStoreDos, useCounter)
+
+expectType<{
+  nameStore: () => MainStore
+  dosStore: () => DosStore
+  counterStore: () => CounterStore
+}>(computedStores)
+
+expectType<{
+  a: () => 'on' | 'off'
+  upper: () => string
+}>(mapState(useStore, ['a', 'upper']))
+
+expectType<{
+  newA: () => 'on' | 'off'
+  newUpper: () => string
+}>(mapState(useStore, { newA: 'a', newUpper: 'upper' }))
+
+expectType<{
+  setToggle: (a: 'on' | 'off') => 'on' | 'off'
+  toggleA: () => void
+}>(mapActions(useStore, ['setToggle', 'toggleA']))
+
+expectType<{
+  newSetToggle: (a: 'on' | 'off') => 'on' | 'off'
+  newToggleA: () => void
+}>(mapActions(useStore, { newSetToggle: 'setToggle', newToggleA: 'toggleA' }))
