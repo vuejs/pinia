b5d27fbd34b006ae7aec01c091d4e246c3fa73fc
Eduardo San Martin Morote <posva13@gmail.com>
2021-04-08 11:02:03 +0200

    feat: add mapActions

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,6 @@ export {
   DefineStoreOptions,
 } from './types'
 
-export { mapStores, mapState, mapGetters } from './mapHelpers'
+export { mapActions, mapStores, mapState, mapGetters } from './mapHelpers'
 // TODO: remove in beta
 export { createStore } from './deprecated'
