8c449d6e52c2690afedd04ea9fa954be9b735233
Eduardo San Martin Morote <posva13@gmail.com>
2021-04-09 12:14:16 +0200

    docs: load theme
    Close #427

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,25 @@ import Theme from 'vitepress/theme'
 import './custom.css'
 import './code-theme.css'
 
-export default {
+const { Layout } = Theme
+
+const existingMounted = Layout.mounted || (() => {})
+
+Layout.mounted = function () {
+  existingMounted.call(this)
+
+  const storedTheme = localStorage.getItem('pinia-color-scheme')
+  const mediaQuery = window.matchMedia('(prefers-color-scheme: light)')
+
+  if (mediaQuery.matches || storedTheme === 'light') {
+    const htmlElement = document.body.parentElement
+    htmlElement.classList.add('light')
+  }
+}
+
+/** @type {import('vitepress').Theme} */
+const config = {
   ...Theme,
 }
+
+export default config
