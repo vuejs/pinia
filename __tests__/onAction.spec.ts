8d99ef8514a3ab09bcc6b212aeb192a6fe146206
Eduardo San Martin Morote <posva13@gmail.com>
2021-05-12 14:19:49 +0200

    test: remove unused code

--- __tests__/onAction.spec.ts
+++ __tests__/onAction.spec.ts
@@ -49,7 +49,6 @@ describe('Subscriptions', () => {
   it('fires callback when action is called', () => {
     const spy = jest.fn()
     store.$onAction(spy)
-    store.$onAction(({}) => {})
     store.direct('Cleiton')
     expect(spy).toHaveBeenCalledTimes(1)
     expect(spy).toHaveBeenCalledWith(
