934c02636772ede21f619dce450739533ec5c5fb
Eduardo San Martin Morote <posva13@gmail.com>
2021-05-18 10:27:29 +0200

    test: ref unwrapping

--- __tests__/state.spec.ts
+++ __tests__/state.spec.ts
@@ -98,8 +98,18 @@ describe('State', () => {
     const store = useStore()
 
     expect(store.name).toBe('Eduardo')
+    expect(store.$state.name).toBe('Eduardo')
+    expect(pinia.state.value.main).toEqual({
+      name: 'Eduardo',
+      counter: 0,
+      double: 0,
+    })
+
     name.value = 'Ed'
     expect(store.name).toBe('Ed')
+    expect(store.$state.name).toBe('Ed')
+    expect(pinia.state.value.main.name).toBe('Ed')
+
     store.name = 'Edu'
     expect(store.name).toBe('Edu')
 
