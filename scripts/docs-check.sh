d73edf20b8753ef016eb332a2a2dff93fd25c468
Eduardo San Martin Morote <posva13@gmail.com>
2021-08-09 23:24:41 +0200

    docs: use regular env var

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -3,4 +3,4 @@
 # check if doc files changes for netlify
 # needed because we cannot use && in netlify.toml
 
-git diff --quiet 'HEAD^' HEAD ./docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite
+git diff --quiet 'HEAD^' HEAD ./docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite && git diff --quiet 'HEAD^' HEAD netlify.toml
