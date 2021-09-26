6b16058d1159cdb3ea4b26dba15d6d3f00880002
Eduardo San Martin Morote <posva13@gmail.com>
2021-09-26 22:48:56 +0200

    docs: upgrade

--- packages/docs/run-typedoc.js
+++ packages/docs/run-typedoc.js
@@ -3,10 +3,12 @@ const path = require('path')
 
 createTypeDocApp({
   name: 'API Documentation',
+  // tsconfig: path.resolve(__dirname, '../../tsconfig.json'),
+  // entryPointStrategy: 'packages',
+  githubPages: false,
   entryPoints: [
     path.resolve(__dirname, '../pinia/src/index.ts'),
     path.resolve(__dirname, '../testing/src/index.ts'),
     path.resolve(__dirname, '../nuxt/src/index.ts'),
   ],
-  entryPointStrategy: 'packages',
 }).build()
