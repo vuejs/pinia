a697bbdb8ff5a866351ae4012ad3c88169fd61f1
Eduardo San Martin Morote <posva13@gmail.com>
2021-09-30 11:03:59 +0200

    build: remove check of old file

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -3,4 +3,4 @@
 # check if doc files changes for netlify
 # needed because we cannot use && in netlify.toml
 
-git diff --quiet 'HEAD^' HEAD ./packages/docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite && git diff --quiet 'HEAD^' HEAD netlify.toml typedoc.js
+git diff --quiet 'HEAD^' HEAD ./packages/docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite && git diff --quiet 'HEAD^' HEAD netlify.toml
