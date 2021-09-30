00e21be38e3b8e1cf73fb1062635c3aa400fddc3
Eduardo San Martin Morote <posva13@gmail.com>
2021-09-30 16:18:21 +0200

    docs: update script check

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -5,4 +5,9 @@
 
 # exit 0 will skip the build while exit 1 will build
 
-git diff --quiet 'HEAD^' HEAD ./packages/docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite && git diff --quiet 'HEAD^' HEAD netlify.toml
+# - check any change in docs
+# - check for new version of vite related deps
+# - changes in netlify conf
+# - a commit message that starts with docs like docs: ... or docs(nuxt): ...
+
+git diff --quiet 'HEAD^' HEAD ./packages/docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite && git diff --quiet 'HEAD^' HEAD netlify.toml && ! git log -1 --pretty=%B | grep '^docs'
