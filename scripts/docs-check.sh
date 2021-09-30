26e319a92400e7008142b17e5996ea880a1806dc
Eduardo San Martin Morote <posva13@gmail.com>
2021-09-30 11:07:22 +0200

    chore: document skip doc

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -3,4 +3,6 @@
 # check if doc files changes for netlify
 # needed because we cannot use && in netlify.toml
 
+# exit 0 will skip the build while exit 1 will build
+
 git diff --quiet 'HEAD^' HEAD ./packages/docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite && git diff --quiet 'HEAD^' HEAD netlify.toml
