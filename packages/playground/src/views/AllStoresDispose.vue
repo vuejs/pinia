bb41a7f21a1326eaaa828f138b2c5ce02ff05c3a
Eduardo San Martin Morote <posva13@gmail.com>
2021-10-27 17:55:30 +0200

    chore: add dispose in playground

---
+++ packages/playground/src/views/AllStoresDispose.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>
+    I have a store "{{ userStore.name }}". I have
+    {{ cartStore.items.length }} items in the cart.
+  </p>
+  <div>
+    <p>Counter: {{ counterStore.double }} = 2 x {{ counterStore.n }}</p>
+    <button @click="counterStore.increment(10)">Increment</button>
+    <button @click="counterStore.fail()">Fail</button>
+    <button @click="counterStore.decrementToZero(300)">Countdown!</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '../stores/user'
+import { useCartStore } from '../stores/cart'
+import { useCounter } from '../stores/counter'
+import { onUnmounted } from 'vue-demi'
+
+const userStore = useUserStore()
+const cartStore = useCartStore()
+const counterStore = useCounter()
+
+onUnmounted(() => {
+  userStore.$dispose()
+  cartStore.$dispose()
+  counterStore.$dispose()
+})
+</script>
