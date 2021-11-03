605cc7cc12256a8d371375423033eb66a3227149
Pooya Parsa <pyapar@gmail.com>
2021-11-03 17:44:42 +0100

    fix: use cjs proxy

--- packages/nuxt/module.cjs
+++ packages/nuxt/module.cjs
@@ -1,24 +1,4 @@
-'use strict';
-
-const kit = require('@nuxt/kit');
-const vueDemi = require('vue-demi');
-
-const module$1 = kit.defineNuxtModule({
-  name: "pinia",
-  configKey: "pinia",
-  defaults: {
-    disableVuex: true
-  },
-  setup(options, nuxt) {
-    if (nuxt.options.features && options.disableVuex) {
-      nuxt.options.features.store = false;
-    }
-    nuxt.options.alias.pinia = "pinia/dist/pinia.mjs";
-    kit.addPlugin({ src: require.resolve("./templates/plugin.mjs") });
-    if (vueDemi.isVue2 && !nuxt.options.build.transpile.includes("pinia")) {
-      nuxt.options.build.transpile.push("pinia");
-    }
-  }
-});
-
-module.exports = module$1;
+// CommonJS proxy to bypass jiti transforms from nuxt 2 and using native ESM
+module.exports = function(...args) {
+  return import('./dist/module.mjs').then(m => m.default.call(this, ...args))
+}
