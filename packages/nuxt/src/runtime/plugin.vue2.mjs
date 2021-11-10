49db94c1aaf858b1726d36df18e49c5fe7370b92
Pooya Parsa <pyapar@gmail.com>
2021-11-11 00:18:35 +0100

    fix: remove $nuxt injection

--- packages/nuxt/src/runtime/plugin.vue2.mjs
+++ packages/nuxt/src/runtime/plugin.vue2.mjs
@@ -10,11 +10,6 @@ export default function (context, inject) {
   inject('pinia', pinia)
   setActivePinia(pinia)
 
-  pinia._p.push(({ store }) => {
-    // Make it non enumerable so it avoids any serialization and devtools
-    Object.defineProperty(store, '$nuxt', { value: context })
-  })
-
   // Hydrate state
   if (process.server) {
     context.beforeNuxtRender(({ nuxtState }) => {
