47e0c926d80c9bbbe84b7afd9e235b8fa7838c26
Eduardo San Martin Morote <posva13@gmail.com>
2021-12-20 20:22:40 +0100

    build: fix build options
    Fix #912

--- packages/nuxt/build.config.ts
+++ packages/nuxt/build.config.ts
@@ -2,8 +2,10 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
-  emitCJS: false,
-  cjsBridge: true,
+  rollup: {
+    emitCJS: false,
+    cjsBridge: true,
+  },
   entries: ['src/module', 'src/templates/plugin'],
   externals: ['pinia', '@nuxt/kit-edge', '@nuxt/types'],
 })
