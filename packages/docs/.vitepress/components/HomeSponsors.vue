4c77e705c3b8b327fea39e2bfc67581b1ddd312d
Eduardo San Martin Morote <posva13@gmail.com>
2022-01-04 23:37:24 +0100

    docs: fix titles sponsors

--- packages/docs/.vitepress/components/HomeSponsors.vue
+++ packages/docs/.vitepress/components/HomeSponsors.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="sponsors_outer">
     <div>
-      <HomeSponsorsGroup v-if="sponsors.platinum" name="Platinum" size="96" />
+      <HomeSponsorsGroup v-if="sponsors.platinum.length" name="Platinum" size="96" />
 
-      <HomeSponsorsGroup v-if="sponsors.gold" name="Gold" size="38" />
+      <HomeSponsorsGroup v-if="sponsors.gold.length" name="Gold" size="38" />
 
-      <HomeSponsorsGroup v-if="sponsors.silver" name="Silver" size="24" />
+      <HomeSponsorsGroup v-if="sponsors.silver.length" name="Silver" size="24" />
 
       <div class="cta">
-        <a class="become-sponsor" href="https://github.com/sponsors/posva"
-          >Become a Sponsor!</a
-        >
+        <a class="become-sponsor" href="https://github.com/sponsors/posva">Become a Sponsor!</a>
       </div>
     </div>
   </div>
