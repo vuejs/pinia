f8fd0c847cb15fba60eb4ebc892d2ffef122e9ed
Eduardo San Martin Morote <posva13@gmail.com>
2022-03-31 10:55:30 +0200

    test: add failing tests

--- packages/pinia/__tests__/multipleRoots.spec.ts
+++ packages/pinia/__tests__/multipleRoots.spec.ts
@@ -11,43 +11,17 @@ describe('Multiple Roots', () => {
     })
   }
 
-  it('uses the same root in child components by default', () => {
+  it('provides a pinia to children components', async () => {
     const pinia = createPinia()
-    const useStore = defineMyStore()
+    const useA = defineMyStore()
+    let nestedA!: ReturnType<typeof useA>
 
     const ChildComponent = defineComponent({
       template: 'no',
       setup() {
-        const store = useStore()
-        expect(store.n).toBe(1)
-      },
-    })
-
-    mount(
-      {
-        template: '<ChildComponent />',
-        components: { ChildComponent },
-        setup() {
-          const store = useStore()
-          expect(store.n).toBe(0)
-          store.n++
-        },
-      },
-      { global: { plugins: [pinia] } }
-    )
-
-    const store = useStore()
-    expect(store.n).toBe(1)
-  })
-
-  it('can use a new pinia root for all child components', async () => {
-    const pinia = createPinia()
-    const useStore = defineMyStore()
-
-    const ChildComponent = defineComponent({
-      template: 'no',
-      setup() {
-        const store = useStore()
+        // should use the provided pinia by the parent
+        const store = useA()
+        nestedA = store
         expect(store.n).toBe(0)
       },
     })
@@ -57,7 +31,7 @@ describe('Multiple Roots', () => {
         components: { ChildComponent },
         setup() {
           providePinia(createPinia())
-          const store = useStore()
+          const store = useA()
           expect(store.n).toBe(0)
           store.n++
         },
@@ -65,7 +39,47 @@ describe('Multiple Roots', () => {
       { global: { plugins: [pinia] } }
     )
 
-    const store = useStore()
-    expect(store.n).toBe(0)
+    const store = useA()
+    // should be the parent one
+    expect(store).not.toBe(nestedA)
+    expect(store.n).toBe(1)
+  })
+
+  it('should be able to use plugins', async () => {
+    const pinia = createPinia()
+    const useA = defineMyStore()
+
+    // @ts-expect-error: type not defined
+    pinia.use(() => ({ parent: true }))
+
+    const ChildComponent = defineComponent({
+      template: 'no',
+      setup() {
+        // should use the provided pinia by the parent
+        const store = useA()
+        // @ts-expect-error: not defined
+        expect(store.parent).toBeUndefined()
+        // @ts-expect-error: not defined
+        expect(store.child).toBe(true)
+      },
+    })
+    mount(
+      {
+        template: '<ChildComponent />',
+        components: { ChildComponent },
+        setup() {
+          const pinia = createPinia()
+          // @ts-expect-error: type not defined
+          pinia.use(() => ({ child: true }))
+          providePinia(pinia)
+          const store = useA()
+          // @ts-expect-error: not defined
+          expect(store.child).toBeUndefined()
+          // @ts-expect-error: not defined
+          expect(store.parent).toBe(true)
+        },
+      },
+      { global: { plugins: [pinia] } }
+    )
   })
 })
