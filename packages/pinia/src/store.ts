d0b15fecb5085469241bdd1a36d8b04ba3754150
Eduardo San Martin Morote <posva13@gmail.com>
2022-04-26 08:50:03 +0200

    build: unify vue dependency

--- packages/pinia/src/store.ts
+++ packages/pinia/src/store.ts
@@ -200,6 +200,7 @@ function createSetupStore<
   )
 
   /* istanbul ignore if */
+  // @ts-expect-error: active is an internal property
   if (__DEV__ && !pinia._e.active) {
     throw new Error('Pinia destroyed')
   }
