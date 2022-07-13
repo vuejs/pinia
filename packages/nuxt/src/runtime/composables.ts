42be2fc22aa99353821d9595061ca991d42127ff
Eduardo San Martin Morote <posva13@gmail.com>
2022-07-13 12:11:22 +0200

    feat(nuxt): add `autoImports` option in module

--- packages/nuxt/src/runtime/composables.ts
+++ packages/nuxt/src/runtime/composables.ts
@@ -1,6 +1,4 @@
 import { useNuxtApp } from '#imports'
-import { defineStore } from 'pinia'
+export * from 'pinia'
 
 export const usePinia = () => useNuxtApp().$pinia
-
-export const definePiniaStore = defineStore
