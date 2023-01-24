0ff7ddd4ff00975fcdda1db77c1063af569d06d6
Eduardo San Martin Morote <posva13@gmail.com>
2023-01-24 08:45:47 +0100

    chore: comment

--- packages/pinia/src/store.ts
+++ packages/pinia/src/store.ts
@@ -397,7 +397,7 @@ function createSetupStore<
           })
       }
 
-      // allow the afterCallback to override the return value
+      // trigger after callbacks
       triggerSubscriptions(afterCallbackList, ret)
       return ret
     }
