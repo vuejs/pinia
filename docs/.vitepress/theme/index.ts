2803143a02f606f0d77203c0d9b93194afcdc3ab
Niceplugin <niceplugin@gmail.com>
2023-03-25 12:57:24 +0900

    docs: sync origin check (#51)
    - add vuemastery pinia weekend promotion banner (#2075)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,37 @@
-import defaultTheme from 'vitepress/theme'
-import MyLayout from './MyLayout.vue'
+import { h, watchEffect } from 'vue'
+import { Theme, useData } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+// import AsideSponsors from './components/AsideSponsors.vue'
+// import AsideSponsors from './components/AsideSponsors.vue'
+import VueMasteryBanner from './components/VueMasteryBanner.vue'
 import './styles/vars.css'
+import VueSchoolLink from './components/VueSchoolLink.vue'
+import VueMasteryLogoLink from './components/VueMasteryLogoLink.vue'
 
-export default {
-  ...defaultTheme,
-  Layout: MyLayout,
+const theme: Theme = {
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      // 'home-features-after': () => h(HomeSponsors),
+      // 'aside-ads-before': () => h(AsideSponsors),
+      // 'layout-top': () => h(VueMasteryBanner),
+    })
+  },
+
+  enhanceApp({ app }) {
+    app.component('VueSchoolLink', VueSchoolLink)
+    app.component('VueMasteryLogoLink', VueMasteryLogoLink)
+  },
+
+  // TODO: real date
+  // setup() {
+  //   const { lang } = useData()
+  //   watchEffect(() => {
+  //     if (typeof document !== 'undefined') {
+  //       document.cookie = `nf_lang=${lang.value}; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/`
+  //     }
+  //   })
+  // },
 }
+
+export default theme
