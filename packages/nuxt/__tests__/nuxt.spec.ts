63c4f6def2468c59c3d66b8ca8c1b42a4ff45dfb
Daniel Roe <daniel@roe.dev>
2023-05-04 09:03:06 +0100

    test: reenable nuxt test

--- packages/nuxt/__tests__/nuxt.spec.ts
+++ packages/nuxt/__tests__/nuxt.spec.ts
@@ -2,42 +2,30 @@
  * @vitest-env node
  */
 import { fileURLToPath } from 'node:url'
-import { describe, it, expect, beforeAll } from 'vitest'
-import {
-  setup,
-  $fetch,
-  useTestContext,
-  startServer,
-  loadFixture,
-} from '@nuxt/test-utils'
+import { describe, it, expect } from 'vitest'
+import { setup, $fetch } from '@nuxt/test-utils'
+
+await setup({
+  server: true,
+  rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+  nuxtConfig: {
+    hooks: {
+      'vite:extendConfig'(config, { isClient }) {
+        config.define!.__BROWSER__ = isClient
+      },
+    },
+    vite: {
+      define: {
+        __DEV__: false,
+        __TEST__: true,
+        __FEATURE_PROD_DEVTOOLS__: false,
+      },
+    },
+  },
+})
 
 describe('works with nuxt', async () => {
-  beforeAll(async () => {
-    await setup({
-      server: true,
-      rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
-      nuxtConfig: {
-        hooks: {
-          'vite:extendConfig'(config, { isClient }) {
-            config.define!.__BROWSER__ = isClient
-          },
-        },
-        vite: {
-          define: {
-            __DEV__: false,
-            __TEST__: true,
-            __FEATURE_PROD_DEVTOOLS__: false,
-          },
-        },
-      },
-    })
-    // await loadFixture()
-    // const ctx = useTestContext()
-    // await startServer()
-  })
-
-  // FIXME: this is consistently failing, seems to not be configured well with nuxt
-  it.skip('works on ssr', async () => {
+  it('works on ssr', async () => {
     const html = await $fetch('/')
     expect(html).toContain('Count: 101')
   })
