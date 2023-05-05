68e1f664d5b3df366aa81dae17ad704acb2ce700
Israel Ugbodaga <israelugbodaga@gmail.com>
2023-05-05 16:52:30 +0100

    remove vuemastery banner

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,20 +3,18 @@ import { Theme, useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 // import AsideSponsors from './components/AsideSponsors.vue'
 // import AsideSponsors from './components/AsideSponsors.vue'
-import VueMasteryBanner from './components/VueMasteryBanner.vue'
 import './styles/vars.css'
 import VueSchoolLink from './components/VueSchoolLink.vue'
 import VueMasteryLogoLink from './components/VueMasteryLogoLink.vue'
 
 const theme: Theme = {
   ...DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      // 'home-features-after': () => h(HomeSponsors),
-      // 'aside-ads-before': () => h(AsideSponsors),
-      'layout-top': () => h(VueMasteryBanner),
-    })
-  },
+  // Layout() {
+  //   return h(DefaultTheme.Layout, null, {
+  //     // 'home-features-after': () => h(HomeSponsors),
+  //     // 'aside-ads-before': () => h(AsideSponsors),
+  //   })
+  // },
 
   enhanceApp({ app }) {
     app.component('VueSchoolLink', VueSchoolLink)
