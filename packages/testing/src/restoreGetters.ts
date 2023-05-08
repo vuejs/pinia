ee0e9ff499e1227337bbe02ad38cdd83d82f1fb9
Eduardo San Martin Morote <posva13@gmail.com>
2023-05-08 20:58:51 +0200

    chore: remove test code

--- packages/testing/src/restoreGetters.ts
+++ packages/testing/src/restoreGetters.ts
@@ -1,10 +1,4 @@
-import {
-  Store,
-  StateTree,
-  _ExtractGettersFromSetupStore_Keys,
-  defineStore,
-} from 'pinia'
-import { ref, computed } from 'vue-demi'
+import { Store, StateTree, _ExtractGettersFromSetupStore_Keys } from 'pinia'
 
 export function restoreGetter<G>(
   store: Store<string, StateTree, G, any>,
@@ -17,22 +11,4 @@ export function restoreGetter<SS>(
 export function restoreGetter<G>(store: Store, getter: any): void {
   // @ts-expect-error: private api
   store[getter] = undefined
-}
-
-function tds() {
-  const s1 = defineStore('s1', () => {
-    const n = ref(0)
-    const double = computed(() => n.value * 2)
-    return { n, double }
-  })()
-
-  const s2 = defineStore('s2', {
-    state: () => ({ n: 0 }),
-    getters: {
-      double: (state) => state.n * 2,
-    },
-  })()
-
-  restoreGetter(s1, 'double')
-  restoreGetter(s2, 'double')
 }
