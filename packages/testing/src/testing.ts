9477abcabd3c4ac703e779d2ecc16c07bc4c58fd
Eduardo San Martin Morote <posva13@gmail.com>
2023-05-11 11:50:46 +0200

    style: lint

--- packages/testing/src/testing.ts
+++ packages/testing/src/testing.ts
@@ -65,7 +65,7 @@ export interface TestingOptions {
 
   /**
    * Function used to create a spy for actions and `$patch()`. Pre-configured
-   * with `jest.fn` in Jest projects or `vi.fn` in Vitest projects if 
+   * with `jest.fn` in Jest projects or `vi.fn` in Vitest projects if
    * `globals: true` is set.
    */
   createSpy?: (fn?: (...args: any[]) => any) => (...args: any[]) => any
