709ed3b3a4787c3a9b5a59f863558b480a96eaaf
Eduardo San Martin Morote <posva13@gmail.com>
2023-07-26 16:37:31 +0200

    fix(devtools): preserve store reactivity

--- packages/pinia/src/devtools/actions.ts
+++ packages/pinia/src/devtools/actions.ts
@@ -47,10 +47,7 @@ export async function actionGlobalCopyState(pinia: Pinia) {
 export async function actionGlobalPasteState(pinia: Pinia) {
   if (checkClipboardAccess()) return
   try {
-    Object.assign(
-      pinia.state.value,
-      JSON.parse(await navigator.clipboard.readText())
-    )
+    loadStoresState(pinia, JSON.parse(await navigator.clipboard.readText()))
     toastMessage('Global state pasted from clipboard.')
   } catch (error) {
     if (checkNotFocusedError(error)) return
@@ -111,7 +108,7 @@ export async function actionGlobalOpenStateFile(pinia: Pinia) {
     const result = await open()
     if (!result) return
     const { text, file } = result
-    Object.assign(pinia.state.value, JSON.parse(text))
+    loadStoresState(pinia, JSON.parse(text))
     toastMessage(`Global state imported from "${file.name}".`)
   } catch (error) {
     toastMessage(
@@ -119,5 +116,14 @@ export async function actionGlobalOpenStateFile(pinia: Pinia) {
       'error'
     )
     console.error(error)
+  }
+}
+
+function loadStoresState(pinia: Pinia, state: Record<string, unknown>) {
+  for (const key in state) {
+    const storeState = pinia.state.value[key]
+    if (storeState) {
+      Object.assign(storeState, state[key])
+    }
   }
 }
