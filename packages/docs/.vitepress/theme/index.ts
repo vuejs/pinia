704a962041dff88df54da0d30194e28b63a579f5
Israel Ugbodaga <israelugbodaga@gmail.com>
2023-12-21 22:14:19 +0100

    feat: add vuemastery banner

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -7,6 +7,7 @@ import './styles/vars.css'
 import './styles/playground-links.css'
 import VueSchoolLink from './components/VueSchoolLink.vue'
 import VueMasteryLogoLink from './components/VueMasteryLogoLink.vue'
+import VueMasteryBanner from './components/VueMasteryBanner.vue'
 
 const theme: Theme = {
   ...DefaultTheme,
@@ -14,6 +15,7 @@ const theme: Theme = {
     return h(DefaultTheme.Layout, null, {
       // 'home-features-after': () => h(HomeSponsors),
       'aside-ads-before': () => h(AsideSponsors),
+      'layout-top': () => h(VueMasteryBanner),
     })
   },
 
