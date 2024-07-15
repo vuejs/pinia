610b2a296aa19f314270fa747c9cfa732616f9b9
zhangmo8 <wegi866@gmail.com>
2024-07-15 16:52:52 +0800

    fix(docs): fix the Chinese api docs

--- packages/docs/.vitepress/config/zh.ts
+++ packages/docs/.vitepress/config/zh.ts
@@ -64,11 +64,11 @@ export const zhConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
         {
           text: 'packages',
           items: [
-            { text: 'pinia', link: '/zh/api/pinia/' },
-            { text: '@pinia/nuxt', link: '/zh/api/@pinia/nuxt/' },
+            { text: 'pinia', link: '/zh/api/modules/pinia/' },
+            { text: '@pinia/nuxt', link: '/zh/api/modules/pinia_nuxt/' },
             {
               text: '@pinia/testing',
-              link: '/zh/api/@pinia/testing/',
+              link: '/zh/api/modules/pinia_testing/',
             },
           ],
         },
