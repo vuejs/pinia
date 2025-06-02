1438aa426dc8d6999b81d19c78510cbb0e00ce91
Eduardo San Martin Morote <posva13@gmail.com>
2025-06-02 09:49:04 +0200

    docs: remove banner

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -12,7 +12,6 @@ import VueSchoolLink from './components/VueSchoolLink.vue'
 import VueMasteryLogoLink from './components/VueMasteryLogoLink.vue'
 import MasteringPiniaLink from './components/MasteringPiniaLink.vue'
 import status from '../translation-status.json'
-import MadVueBanner from './components/MadVueBanner.vue'
 
 const i18nLabels = {
   zh: '该翻译已同步到了 ${date} 的版本，其对应的 commit hash 是 <code>${hash}</code>。',
@@ -27,7 +26,6 @@ const theme: Theme = {
       'aside-ads-before': () => h(AsideSponsors),
       // 'layout-top': () => h(VuejsdeConfBanner),
       'doc-before': () => h(TranslationStatus, { status, i18nLabels }),
-      'layout-top': () => h(MadVueBanner),
     })
   },
 
