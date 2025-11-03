d89614a38df9ecd69af2b906d29125fda87fe2f6
Eduardo San Martin Morote <posva13@gmail.com>
2025-11-03 11:02:07 +0100

    test: name

--- packages/nuxt/test/nuxt.spec.ts
+++ packages/nuxt/test/nuxt.spec.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils/e2e'
 
-describe('works with nuxt', async () => {
+describe('Nuxt', async () => {
   await setup({
     server: true,
     rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
