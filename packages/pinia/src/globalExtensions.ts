55dbfc5c20d4461748996aa74d8c0913e89fb98e
skirtle <65301168+skirtles-code@users.noreply.github.com>
2026-01-27 08:24:16 +0000

    refactor(types): remove Vue 2 type extensions (#3098)

--- packages/pinia/src/globalExtensions.ts
+++ packages/pinia/src/globalExtensions.ts
@@ -1,38 +1,9 @@
 import type { Pinia } from './rootStore'
-import type { Store, StoreGeneric } from './types'
+import type { StoreGeneric } from './types'
 
 // Extensions of Vue types to be appended manually
 // https://github.com/microsoft/rushstack/issues/2090
 // https://github.com/microsoft/rushstack/issues/1709
-
-// @ts-ignore: works on Vue 2, fails in Vue 3
-declare module 'vue/types/vue' {
-  interface Vue {
-    /**
-     * Currently installed pinia instance.
-     */
-    $pinia: Pinia
-
-    /**
-     * Cache of stores instantiated by the current instance. Used by map
-     * helpers. Used internally by Pinia.
-     *
-     * @internal
-     */
-    _pStores?: Record<string, Store>
-  }
-}
-
-// @ts-ignore: works on Vue 2, fails in Vue 3
-declare module 'vue/types/options' {
-  interface ComponentOptions<V> {
-    /**
-     * Pinia instance to install in your application. Should be passed to the
-     * root Vue.
-     */
-    pinia?: Pinia
-  }
-}
 
 /**
  * NOTE: Used to be `@vue/runtime-core` but it break types from time to time. Then, in Vue docs, we started recommending
