85f21cf9698db25a334768e024e78c5b2b6a2e77
Ragnar-Oock <ugo.dumontot@gmail.com>
2026-03-31 23:42:10 +0200

    test: add test suite for the new context API

--- packages/pinia/__tests__/storeContext.spec.ts
+++ packages/pinia/__tests__/storeContext.spec.ts
@@ -1,5 +1,101 @@
-import { it,  describe } from 'vitest';
+import { mount } from '@vue/test-utils'
+import { createPinia, defineStore } from 'pinia'
+import { it, describe, expect } from 'vitest'
 
-describe('storeContext.spec.ts', () => {
-	$END$
+const key = Symbol('key')
+declare module '../src' {
+  export interface PiniaSetupContext {
+    value: string
+    [key]: string
+  }
+}
+describe('store context injection', () => {
+  it('provides context to setup stores', async ({ task: { id } }) => {
+    const pinia = createPinia().provide('value', id).provide(key, id)
+
+    mount({ template: 'none' }, { global: { plugins: [pinia] } })
+
+    const useStore = defineStore('main', ({ context }) => {
+      return {
+        value: context.value,
+        key: context[key],
+      }
+    })
+
+    const store = useStore(pinia)
+
+    expect(store).toHaveProperty('value', id)
+    expect(store).toHaveProperty('key', id)
+  })
+
+  it('provides context to option state', ({ task: { id } }) => {
+    const pinia = createPinia().provide('value', id).provide(key, id)
+
+    mount({ template: 'none' }, { global: { plugins: [pinia] } })
+
+    const useStore = defineStore('main', {
+      state: (context) => ({
+        value: context.value,
+        key: context[key],
+      }),
+    })
+
+    const store = useStore(pinia)
+
+    expect(store).toHaveProperty('value', id)
+  })
+
+  it('is not writable', ({ task: { id } }) => {
+    const pinia = createPinia().provide('value', id).provide(key, id)
+
+    mount({ template: 'none' }, { global: { plugins: [pinia] } })
+
+    const useStore = defineStore('main', ({ context }) => {
+      let hasThrown = false
+
+      try {
+        // @ts-ignore we are making sure context is readonly
+        context.value = 'not writable'
+      } catch (e) {
+        hasThrown = true
+      }
+
+      return {
+        hasThrown,
+      }
+    })
+
+    const store = useStore(pinia)
+
+    expect(store).toHaveProperty('hasThrown', true)
+  })
+
+  it('has the same value for all store', ({ task: { id } }) => {
+    const pinia = createPinia().provide('value', id)
+
+    const useStoreA = defineStore('A', ({ context }) => {
+      return {
+        value: context.value,
+      }
+    })
+    const useStoreB = defineStore('A', ({ context }) => {
+      return {
+        value: context.value,
+      }
+    })
+
+    const storeA = useStoreA(pinia)
+    const storeB = useStoreB(pinia)
+
+    expect(storeA).toHaveProperty('value', id)
+    expect(storeB).toHaveProperty('value', id)
+  })
+
+  it('cannot provide the same key multiple times', () => {
+    const pinia = createPinia()
+      .provide('value', 'first')
+      .provide('value', 'second')
+
+    expect(pinia._i).toHaveProperty('value', 'first')
+  })
 })
