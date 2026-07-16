b0249de0bde14cef7d7b5727e10ee44674ad4b2a
Eduardo San Martin Morote <posva13@gmail.com>
2026-07-16 11:25:17 +0200

    fix(types): avoid reserved .vue at the end of name
    Fix #3160
    
    .vue is treated as a vue component, so the dts generation tries to match
    a plugin.vue component that doesn't exist

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -57,7 +57,7 @@ const module: NuxtModule<ModuleOptions> = defineNuxtModule<ModuleOptions>({
     // Add runtime plugin before the router plugin
     // https://github.com/nuxt/framework/issues/9130
     nuxt.hook('modules:done', () => {
-      addPlugin(resolve(runtimeDir, 'plugin.vue'))
+      addPlugin(resolve(runtimeDir, 'plugin'))
       addPlugin(resolve(runtimeDir, 'payload-plugin'))
     })
 
