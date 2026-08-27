b71f8deb9cf54b3450bd24769709aef2c032a3ae
Eduardo San Martin Morote <posva13@gmail.com>
2026-08-27 12:39:18 +0200

    chore: up size tooling

--- packages/size-check/scripts/check-size.mjs
+++ packages/size-check/scripts/check-size.mjs
@@ -2,9 +2,8 @@
 import fs from 'node:fs'
 import { globby } from 'globby'
 import path from 'node:path'
-import { gzipSync } from 'node:zlib'
+import { brotliCompressSync, gzipSync } from 'node:zlib'
 import { fileURLToPath } from 'node:url'
-import { compress } from 'brotli-wasm'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -17,7 +16,7 @@ async function checkFileSize(filePath) {
   const minSize = (file.length / 1024).toFixed(2) + 'kb'
   const gzipped = gzipSync(file)
   const gzippedSize = (gzipped.length / 1024).toFixed(2) + 'kb'
-  const compressed = await compress(file)
+  const compressed = brotliCompressSync(file)
   const compressedSize = (compressed.length / 1024).toFixed(2) + 'kb'
   console.log(
     `\x1b[1;90m${path.basename(filePath)}\x1b[0m min:${minSize} / gzip:${gzippedSize} / brotli:${compressedSize}`
